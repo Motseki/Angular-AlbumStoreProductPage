c349218127963aabe4aaa20c8810c6c21013816f
Lebohang Motseki <Lebohang Motseki>
2021-11-11 17:03:59 +0200

    Part3

---
+++ src/app/product.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+  albumInfo;
+
+  private _albumUrl = '../assets/album.json'; 
+  private _productService: any;
+
+  constructor(private _http: Http) { }
+
+
+
+  getAlbum(id:number){
+  return this._http.get(this._albumUrl).map(response => response.json());
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+  }
+ 
+}
