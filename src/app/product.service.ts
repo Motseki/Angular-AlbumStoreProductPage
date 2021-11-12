cde8d3e99a28b3015a1145d613c058d073045cac
Lebohang Motseki <Lebohang Motseki>
2021-11-12 17:15:23 +0200

    Part 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,21 +3,25 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   albumInfo;
 
+  private _productsUrl = '../assets/products.json';
   private _albumUrl = '../assets/album.json'; 
   private _productService: any;
 
   constructor(private _http: Http) { }
 
-
-
   getAlbum(id:number):Observable<Album>{
   return this._http.get(this._albumUrl).map(response => <Album>response.json());
     //this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
+  }
+
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
   }
  
 }
